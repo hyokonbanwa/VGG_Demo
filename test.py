3105e3ba691d73be8ff64ca23771cae2d438dd1f
SOTA ITO <131466870+s-ito0621@users.noreply.github.com>
2023-11-09 20:43:35 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,59 @@
+
+
+import traitlets
+from jetcam.utils import bgr8_to_jpeg
+from IPython.display import display
+import ipywidgets
+from jetcam.csi_camera import CSICamera
+import cv2
+import torch
+from torch import nn, optim
+import numpy as np
+import random
+import sys
+from src.vgg_local import VGG_LOCAL
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from PIL import Image
+import gc
+
+model_path = "./final_weight.pth"
+model_ver = "VGG11"
+image_path = "image.jpg"
+RPS_list = ["チョキ", "グー", "パー"]
+
+image = cv2.imread(image_path) 
+
+print("撮影が終わりました")
+
+image_size = 64
+# Set random seed for reproducibility
+random_seed = 9999
+random.seed(random_seed)
+np.random.seed(random_seed)
+torch.manual_seed(random_seed)
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+    torch.cuda.manual_seed(random_seed)
+    torch.backends.cudnn.deterministic = True
+else:
+    device = torch.device('cpu')
+# Preprocessing for test data
+test_transform = transforms.Compose([
+    transforms.Resize((image_size, image_size)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+
+
+input = test_transform(Image.fromarray(image))
+# Initialize and load model weights
+model = VGG_LOCAL(model_ver, classes=3, image_size=image_size).to(device)
+model.load_state_dict(torch.load(model_path))
+print("モデルの読み込み完了．推論開始")
+model.eval()
+with torch.no_grad():
+    input = input.to(device)
+    output = model(input.unsqueeze(0))
+
+print(f'{RPS_list[torch.argmax(output)]}です')
