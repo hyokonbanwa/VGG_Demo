49d2d953f8c27f3fad7b553c949f23de62f42d57
hyokonbnawa <hyokonbnawa@outlook.jp>
2024-10-14 08:37:55 -0600

    setup.shを修正

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ git clone https://github.com/NVIDIA-AI-IOT/jetcam
 cd jetcam
 pip3 install traitlets
 sudo python setup.py bdist_wheel
- pip install ./dist/jetcam-0.0.0-py3-none-any.whl --user
+pip install ./dist/jetcam-0.0.0-py3-none-any.whl --user
 sudo pip3 install jupyter
 cd ..
